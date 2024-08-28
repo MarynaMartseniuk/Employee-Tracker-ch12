75b3b47bb130f49fc143ca7e1f20b16bdf67c4a0
MarcM <marina.ved80gmail.com>
2024-08-28 12:00:54 -0600

    add seeds

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Employee-Tracker-ch12
 a command-line application to manage a company's employee database, using Node.js, Inquirer, and PostgreSQL.
+
+## Description
+### Motivation to create this App 
+### I built this App because 
+### This App hepls 
+### This project was interesting and usefull for me because, as a begginer, I learned how to use NodeJs, Inquirer package, Mardown syntax.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+### To use this App you need 
+
+## Usage
+
+
+### run the App will look like this:
+![run the App will look like this:]()
+
+
+## Credits
+1. sorce of code: UofU bootcamp, module 12, activity 23 
+
+## License
+### All assets and code are under the MIT license. Please, check more info in lisence section of this repo.
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## How to Contribute
+### this section is comming soon
+
+## Tests
+### this section is comming soon
+
+## Questions
+### If you have any questions, please, contact me using my contact info bellow:
+- my GitHub name: MarynaMartseniuk
+- my GitHub link: https://github.com/MarynaMartseniuk
+- my email address: marina.ved80@gmail.com
---
+++ db/seeds.sql
@@ -0,0 +1,54 @@
+INSERT INTO departments (name)
+VALUES ('Workshop'), 
+       ('Human Resources'),
+       ('IT'),
+       ('Cafeteria'),
+       ('Office'),
+       ('Security'),
+       ('Medical');
+
+-- how I will know a department_id ?????
+INSERT INTO role (title, salary, department_id)
+       -- rol_id 1
+VALUES ('operator', 2500, 1),
+       -- rol_id 2
+       ('mechanic', 3500, 1),
+       -- rol_id 3
+       ('Line Lead', 4000, 1),
+       -- rol_id 4
+       ('Coach', 5500, 1),
+       -- rol_id 5
+       ('HR Officer', 4000, 2),
+       -- rol_id 6
+       ('IT Specialist', 4000, 3),
+       -- rol_id 7
+       ('Cafeteria Officer', 3000, 4),
+       -- rol_id 8
+       ('Chief', 5000, 4),
+       -- rol_id 9
+       ('Office Manager', 4500, 5),
+       -- rol_id 10
+       ('Senior Office Manager', 6000, 5),
+       -- rol_id 11
+       ('Security Officer', 3000, 6),
+       -- rol_id 12
+       ('Senior Security Officer', 4500, 6),
+       -- rol_id 13
+       ('Nurse', 5000, 7);
+
+INSERT INTO role (first_name, last_name, role_id, manager_id)
+VALUES ('Q', 'Q', 1, 3),
+       ('W', 'W', 1, 3),
+       ('E', 'E', 2, 3),
+       ('R', 'R', 3, 4),
+       ('T', 'T', 4, 10),
+       ('Y', 'Y', 5, 0),
+       ('U', 'U', 6, 0),
+       ('I', 'I', 7, 8),
+       ('O', 'O', 8, 0),
+       ('P', 'P', 9, 10),
+       ('A', 'A', 10, 0),
+       ('S', 'S', 11, 12),
+       ('D', 'D', 11, 12),
+       ('F', 'F', 12, 0),
+       ('G', 'G', 13, 0);
